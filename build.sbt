2c30de1c82af92459b4c7096432b76dc7617b539
vicaba <victor.caballero.p@outlook.com>
2023-08-15 12:22:10 +0200

    feat(copy-file): copy file v1 finished

--- build.sbt
+++ build.sbt
@@ -4,8 +4,14 @@ ThisBuild / scalaVersion := "3.3.0"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "cats-overview"
+    name := "cats-tutorial"
   )
 
 libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.1"
 
+scalacOptions ++= Seq(
+  "-feature",
+  "-deprecation",
+  "-unchecked",
+  "-language:postfixOps"
+)
---
+++ src/main/scala/cats_effect/CopyFile.scala
@@ -0,0 +1,54 @@
+package cats_effect
+
+import java.io.*
+import cats.effect._
+
+// https://typelevel.org/cats-effect/docs/tutorial
+object CopyFile extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- if (args.length < 2) IO.raiseError(new IllegalArgumentException("Need origin and destination files"))
+      else IO.unit
+      orig = new File(args(0))
+      dest = new File(args(1))
+      count <- copy(orig, dest)
+      _ <- IO.println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}")
+    } yield ExitCode.Success
+
+  def copy(origin: File, destination: File): IO[Long] =
+    inputOutputStreams(origin, destination).use { case (in, out) =>
+    transfer(in, out)
+  }
+
+  private def inputOutputStreams(in: File, out: File): Resource[IO, (InputStream, OutputStream)] =
+    for {
+      inStream <- inputStream(in)
+      outStream <- outputStream(out)
+    } yield (inStream, outStream)
+
+  private def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
+    transmit(origin, destination, new Array[Byte](1024 * 10), 0L)
+
+  private def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+    for {
+      amount <- IO.blocking(origin.read(buffer, 0, buffer.size))
+      count  <- if(amount > -1) IO.blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+      else IO.pure(acc) // End of read stream reached (by java.io.InputStream contract), nothing to write
+    } yield count // Returns the actual amount of bytes transmitted // Returns the actual amount of bytes transmitted
+
+  private def inputStream(f: File): Resource[IO, FileInputStream] =
+    Resource.make {
+      IO.blocking(new FileInputStream(f)) // build
+    } { inStream =>
+      IO.blocking(inStream.close()).handleErrorWith(_ => IO.unit) // release
+    }
+
+  private def outputStream(f: File): Resource[IO, FileOutputStream] =
+    Resource.make {
+      IO.blocking(new FileOutputStream(f)) // build
+    } { outStream =>
+      IO.blocking(outStream.close()).handleErrorWith(_ => IO.unit) // release
+    }
+
+}
